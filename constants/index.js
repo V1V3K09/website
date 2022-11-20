df818ac920f74b5191e70289110babb6bd24b54c
vivek <113044699+V1V3K09@users.noreply.github.com>
2022-11-20 11:43:22 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,24 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Gallery",
+  },
+  {
+    id: "product",
+    title: "Companies",
+  },
+  {
+    id: "clients",
+    title: "Faq's",
+  },
+  {
+    id: "clients",
+    title: "Contact Us",
+  },
+];
